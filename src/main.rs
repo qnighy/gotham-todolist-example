b1fa20c588c54fbb04b7cba674124ce3c7c71a9c
Masaki Hara <ackie.h.gmai@gmail.com>
2018-04-29 10:39:30 +0900

    Use handlebars.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![feature(proc_macro, generators)]
 
+#[macro_use]
+extern crate serde_json;
 extern crate futures_await as futures;
 extern crate gotham;
+extern crate handlebars_gotham;
 extern crate hyper;
 extern crate mime;
 extern crate tokio;
@@ -11,26 +14,37 @@ use tokio::prelude::*;
 
 use gotham::handler::HandlerError;
 use gotham::http::response::create_response;
+use gotham::pipeline::new_pipeline;
+use gotham::pipeline::single::single_pipeline;
 use gotham::router::builder::*;
 use gotham::router::Router;
 use gotham::state::State;
-use hyper::{Response, StatusCode};
+use handlebars_gotham::{DirectorySource, HandlebarsEngine, Template};
+use hyper::{Method, Response, StatusCode};
+use serde_json::{Map, Value};
 
 type HandlerResult = Result<(State, Response), (State, HandlerError)>;
 
 fn router() -> Router {
-    build_simple_router(|route| {
+    let hbse = HandlebarsEngine::new(vec![Box::new(DirectorySource::new("./templates/", ".hbs"))]);
+    hbse.reload().unwrap();
+
+    let (chain, pipelines) = single_pipeline(new_pipeline().add(hbse).build());
+    build_router(chain, pipelines, |route| {
         route.get_or_head("/").to(index);
     })
 }
 
 #[async(boxed)]
 fn index(state: State) -> HandlerResult {
-    let response = create_response(
-        &state,
-        StatusCode::Ok,
-        Some(("Hello, world!".to_string().into_bytes(), mime::TEXT_PLAIN)),
-    );
+    let mut state = state;
+    let method = state.borrow::<Method>().clone();
+    if method == Method::Get {
+        state.put(Template::new("index", &json!({})));
+    }
+
+    let response = create_response(&state, StatusCode::Ok, None);
+
     Ok((state, response))
 }
 
